b2d8c43a9194d5af59582385e4d7f9ca23bad506
Ivo Georgiev <ivo@linvo.org>
2016-01-10 10:16:44 +0200

    Improvement on error handling

--- guidebox.js
+++ guidebox.js
@@ -94,7 +94,8 @@ function guideboxGet(path, callback) {
         guideboxPrg[path] = [];
 
         needle.get(GUIDEBOX_BASE+path, function(err, resp, body) {
-            if (body.error) { err = body.error; body = null; }
+            if (err) { err = err; body = null; }
+            if (body && body.error) { err = body.error; body = null; }
 
             if (body && body.results) cacheSet("guidebox", path, body, (body.results && body.results.length) ? (path.match("movie") ? 15*DAY : 6*DAY) : 2*DAY);
             
